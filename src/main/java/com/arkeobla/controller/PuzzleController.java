55090630c0c32131cfcade53022a50683dc789c2
Mustafa Can Yılmaz <mcy@Mustafa-MacBook-Pro.local>
2025-12-22 17:35:35 +0300

    Games: Added local puzzle images (5 levels), fixed image loading

--- src/main/java/com/arkeobla/controller/PuzzleController.java
+++ src/main/java/com/arkeobla/controller/PuzzleController.java
@@ -14,21 +14,21 @@ public class PuzzleController {
     private final List<PuzzleLevel> levels = new ArrayList<>();
 
     public PuzzleController() {
-        // Picsum placeholder resimleri kullanıyoruz - CORS sorunsuz çalışır
+        // Yerel statik dosyalar kullanıyoruz - CORS sorunu olmaz
         levels.add(new PuzzleLevel(1, "Antik Mozaik", "3x3 Kolay", 3,
-                "https://picsum.photos/seed/zeugma/500/500"));
+                "/img/puzzle1.jpg"));
 
         levels.add(new PuzzleLevel(2, "Tarihi Yapı", "4x4 Orta", 4,
-                "https://picsum.photos/seed/gobeklitepe/500/500"));
+                "/img/puzzle2.jpg"));
 
         levels.add(new PuzzleLevel(3, "Piramitler", "5x5 Zor", 5,
-                "https://picsum.photos/seed/pyramids/500/500"));
+                "/img/puzzle3.jpg"));
 
         levels.add(new PuzzleLevel(4, "Antik Arena", "6x6 Uzman", 6,
-                "https://picsum.photos/seed/colosseum/500/500"));
+                "/img/puzzle4.jpg"));
 
         levels.add(new PuzzleLevel(5, "Tapınak", "7x7 Usta", 7,
-                "https://picsum.photos/seed/parthenon/500/500"));
+                "/img/puzzle5.jpg"));
     }
 
     @GetMapping("/puzzle")
